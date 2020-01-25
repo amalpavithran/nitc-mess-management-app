0e68eaa3edc46a37584f910b4e7e48272bcc86d9
Amal Pavithran <amalpavithranmp@gmail.com>
2020-01-25 22:11:11 +0530

    minor fix

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import SignupScreen from './Signup';
 
 var url = "http://nitc-mess.anandu.net"
 
-class App extends Component {
+class Signin extends Component {
   constructor(props) {
     super(props);
     this.state = {
